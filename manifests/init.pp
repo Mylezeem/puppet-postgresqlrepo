5424a186567942431e2c6f9772ce8cbc8c4b204b
Yanis Guenane <yguenane@gmail.com>
2014-06-09 12:32:10 -0400

    Initial init.pp commit. Set up the postgresql repositories.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,41 +1,67 @@
 # == Class: postgresqlrepo
 #
-# Full description of class postgresqlrepo here.
+# A puppet module that configure the PostgreSQL repository on an Enterprise Linux (and relatives) system
 #
 # === Parameters
 #
-# Document parameters here.
+# [*version*]
+#   (float) PostgreSQL version number
+#   Possible value: 8.4, 9.0, 9.1, 9.2, 9.3
+#   Default: 9.3
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# [*source*]
+#   (boolean) Should the srpms repository should be enabled.
+#   Default: false
 #
 # === Examples
 #
-#  class { postgresqlrepo:
-#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  include postgresqlrepo
+#
+#    or
+#
+#  class { 'postgresqlrepo' :
+#   version => '9.1',
+#   source  => true,
 #  }
 #
 # === Authors
 #
-# Author Name <author@domain.com>
+# Yanis Guenane <yguenane@gmail.com>
 #
 # === Copyright
 #
-# Copyright 2014 Your name here, unless otherwise noted.
+# Copyright 2014 Yanis Guenane
 #
-class postgresqlrepo {
+class postgresqlrepo (
+  $version        = '9.3',
+  $source         = false,
+) {
 
+  if ! ($::operatingsystem in ['RedHat', 'Fedora', 'CentOS']) {
+    fail ("This module does not support your operating system : ${::operatingsystem}")
+  }
+
+  $os = $::operatingsystem ? {
+    /RedHat|CentOS/ => 'redhat',
+    'Fedora'        => 'fedora',
+  }
+  $os_shortname = $::operatingsystem ? {
+    /RedHat|CentOS/ => 'rhel',
+    'Fedora'        => 'fedora',
+  }
+
+  yumrepo { "pgdg${version}" :
+    baseurl  => "http://yum.postgresql.org/${version}/${os}/${os_shortname}-\$releasever-\$basearch",
+    gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-93',
+    gpgcheck => 1,
+    enabled  => 1,
+  }
+
+  yumrepo { "pgdg${version}-source" :
+    baseurl  => "http://yum.postgresql.org/srpms/${version}/${os}/${os_shortname}-\$releasever-\$basearch",
+    gpgkey   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-93',
+    gpgcheck => 1,
+    enabled  => $source,
+  }
 
 }
