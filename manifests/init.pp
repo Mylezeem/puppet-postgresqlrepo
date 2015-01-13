e2df85680ae14fdd38b1aed7df81bf23b771ebd7
Dimitri Savineau <savineau.dimitri@gmail.com>
2015-01-13 15:36:42 -0500

    Add support to debian/ubuntu distros

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,12 @@
 # == Class: postgresqlrepo
 #
-# A puppet module that configure the PostgreSQL repository on an Enterprise Linux (and relatives) system
+# A puppet module that configure the PostgreSQL repository on a Linux system.
 #
 # === Parameters
 #
 # [*version*]
 #   (float) PostgreSQL version number
-#   Possible value: 8.4, 9.0, 9.1, 9.2, 9.3
+#   Possible value: 8.4, 9.0, 9.1, 9.2, 9.3 and 9.4
 #   Default: 9.3
 #
 # [*repo_enable*]
@@ -14,7 +14,7 @@
 #   Default: true
 #
 # [*repo_source_enable*]
-#   (boolean) Should the srpms repo should be enabled.
+#   (boolean) Should the source repo should be enabled.
 #   Default: false
 #
 # === Examples
@@ -31,6 +31,7 @@
 # === Authors
 #
 # Yanis Guenane <yguenane@gmail.com>
+# Dimitri Savineau <savineau.dimitri@gmail.com>
 #
 # === Copyright
 #
@@ -46,46 +47,60 @@ class postgresqlrepo (
 
   $version_alt = regsubst($version, '^(\d+)\.(\d+)$', '\1\2')
 
-  if ! ($::operatingsystem in ['RedHat', 'Fedora', 'CentOS']) {
-    fail ("This module does not support your operating system : ${::operatingsystem}")
-  }
-
   $os = $::operatingsystem ? {
     'Fedora'        => 'fedora',
     /RedHat|CentOS/ => 'redhat',
+    default         => undef,
   }
   $os_shortname = $::operatingsystem ? {
     'Fedora'        => 'fedora',
     /RedHat|CentOS/ => 'rhel',
+    default         => undef,
   }
 
-  yumrepo { "pgdg${version_alt}" :
-    descr    => "PostgreSQL ${version} \$releasever - \$basearch",
-    baseurl  => "http://yum.postgresql.org/${version}/${os}/${os_shortname}-\$releasever-\$basearch",
-    gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-${version_alt}",
-    gpgcheck => 1,
-    enabled  => bool2num($repo_enable),
-  }
+  case $::operatingsystem {
+    'RedHat','Fedora','CentOS': {
+      yumrepo { "pgdg${version_alt}" :
+        descr    => "PostgreSQL ${version} \$releasever - \$basearch",
+        baseurl  => "http://yum.postgresql.org/${version}/${os}/${os_shortname}-\$releasever-\$basearch",
+        gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-${version_alt}",
+        gpgcheck => 1,
+        enabled  => bool2num($repo_enable),
+      }
 
-  yumrepo { "pgdg${version_alt}-source" :
-    descr    => "PostgreSQL ${version} \$releasever - \$basearch - Source",
-    baseurl  => "http://yum.postgresql.org/srpms/${version}/${os}/${os_shortname}-\$releasever-\$basearch",
-    gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-${version_alt}",
-    gpgcheck => 1,
-    enabled  => bool2num($repo_source_enable),
-  }
+      yumrepo { "pgdg${version_alt}-source" :
+        descr    => "PostgreSQL ${version} \$releasever - \$basearch - Source",
+        baseurl  => "http://yum.postgresql.org/srpms/${version}/${os}/${os_shortname}-\$releasever-\$basearch",
+        gpgkey   => "file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-${version_alt}",
+        gpgcheck => 1,
+        enabled  => bool2num($repo_source_enable),
+      }
 
-  file {"/etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-${version_alt}" :
-    ensure => present,
-    owner  => 'root',
-    group  => 'root',
-    mode   => '0644',
-    source => "puppet:///modules/postgresqlrepo/RPM-GPG-KEY-PGDG-${version_alt}",
-  }
+      file {"/etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-${version_alt}" :
+        ensure => present,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0644',
+        source => "puppet:///modules/postgresqlrepo/RPM-GPG-KEY-PGDG-${version_alt}",
+      }
 
-  postgresqlrepo::rpm_gpg_key {"PGDG-${version_alt}":
-    path   => "/etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-${version_alt}",
-    before => Yumrepo["pgdg${version_alt}", "pgdg${version_alt}-source"],
+      postgresqlrepo::rpm_gpg_key {"PGDG-${version_alt}":
+        path   => "/etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-${version_alt}",
+        before => Yumrepo["pgdg${version_alt}", "pgdg${version_alt}-source"],
+      }
+    }
+    'Debian','Ubuntu': {
+      apt::source { "pgdg${version_alt}":
+        location    => 'http://apt.postgresql.org/pub/repos/apt/',
+        release     => "${::lsbdistcodename}-pgdg",
+        repos       => 'main',
+        key_source  => 'https://www.postgresql.org/media/keys/ACCC4CF8.asc',
+        include_src => $repo_source_enable
+      }
+    }
+    default: {
+      fail ("This module does not support your operating system : ${::operatingsystem}")
+    }
   }
 
 }
--- spec/classes/postgresqlrepo_spec.rb
+++ spec/classes/postgresqlrepo_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe 'postgresqlrepo' do
 
-  ['Fedora', 'RedHat', 'CentOS'].each do |operatingsystem|
+  ['Fedora', 'RedHat', 'CentOS', 'Debian', 'Ubuntu'].each do |operatingsystem|
 
     context "Operating system #{operatingsystem}" do
 
@@ -16,11 +16,17 @@ describe 'postgresqlrepo' do
       when 'CentOS'
         os = 'redhat'
         os_shortname = 'rhel'
+      when 'Debian'
+        codename  = 'wheezy'
+      when 'Ubuntu'
+        codename  = 'precise'
       end
 
       let (:facts) do
         {
-          :operatingsystem => operatingsystem
+          :operatingsystem => operatingsystem,
+          :lsbdistcodename => codename,
+          :lsbdistid       => "#{operatingsystem}"
         }
       end
 
@@ -32,22 +38,35 @@ describe 'postgresqlrepo' do
         }
       end
 
-      it 'setup pgdg93.repo' do
-        should contain_yumrepo('pgdg93').with({
-          'enabled'  => '1',
-          'gpgcheck' => '1',
-          'gpgkey'   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-93',
-          'baseurl'  => "http://yum.postgresql.org/9.3/#{os}/#{os_shortname}-$releasever-$basearch"
-        })
-      end
+      case operatingsystem
+      when 'Fedora','RedHat','CentOS'
+        it 'setup pgdg93.repo' do
+          should contain_yumrepo('pgdg93').with({
+            'enabled'  => '1',
+            'gpgcheck' => '1',
+            'gpgkey'   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-93',
+            'baseurl'  => "http://yum.postgresql.org/9.3/#{os}/#{os_shortname}-$releasever-$basearch"
+          })
+        end
 
-      it 'setup pgdg93-source.repo' do
-        should contain_yumrepo('pgdg93-source').with({
-          'enabled'  => '0',
-          'gpgcheck' => '1',
-          'gpgkey'   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-93',
-          'baseurl'  => "http://yum.postgresql.org/srpms/9.3/#{os}/#{os_shortname}-$releasever-$basearch"
-        })
+        it 'setup pgdg93-source.repo' do
+          should contain_yumrepo('pgdg93-source').with({
+            'enabled'  => '0',
+            'gpgcheck' => '1',
+            'gpgkey'   => 'file:///etc/pki/rpm-gpg/RPM-GPG-KEY-PGDG-93',
+            'baseurl'  => "http://yum.postgresql.org/srpms/9.3/#{os}/#{os_shortname}-$releasever-$basearch"
+          })
+        end
+      when 'Debian','Ubuntu'
+        it 'setup pgdg93.list' do
+          should contain_apt__source('pgdg93').with({
+            'location'    => 'http://apt.postgresql.org/pub/repos/apt/',
+            'release'     => "#{codename}-pgdg",
+            'repos'       => 'main',
+            'key_source'  => 'https://www.postgresql.org/media/keys/ACCC4CF8.asc',
+            'include_src' => false
+          })
+        end
       end
 
     end
